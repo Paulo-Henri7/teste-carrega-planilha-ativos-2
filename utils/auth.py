2cd0a188cd0d8c75fecbfc6c1b9e99f2641c5214
Paulo <paulohenriquetav@gmail.com>
2026-06-22 11:37:59 -0400

    Delete utils directory

--- utils/auth.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-
-def obter_usuario():
-    try:
-        return st.context.headers.get("X-Forwarded-Email", "usuario_desconhecido")
-    except Exception:
-        return "usuario_desconhecido"
